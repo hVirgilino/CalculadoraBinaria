2ceb88f1e27c41ac134aaa871b51028d96ae7b95
Henrique <henriquevirgilino@ucl.br>
2022-04-05 17:17:19 -0300

    Criando menu interativo

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    int u = 163; // código dec da tabela ascii para "ú"
+    int c = 135; // código dec da tabela ascii para "ç"
+    int a = 198; // código dec da tabela ascii para "ã"
+    int A = 160; // código dec da tabela ascii para "á"
+    int o = 162; // código dec da tabela ascii para "ó"
+    int mod = 37; // código dec da tabela ascii para "%"
+
+    printf("Selecione o operador de acordo com o n%cmero apresentado.\n1: + (Soma)\n2: - (Subtra%c%co)\n3: * (Multiplica%c%co)\n4: / (Divis%co)\n5: %c (M%cdulo)\n\nAp%cs isso, basta inserir os n%cmeros (em bin%crio) para receber o resultado.",u,c,a,c,a,a,mod,o,o,u,A);
+
+    int escolha; // recebendo o número da operação
+    scanf("%d", escolha);
+
+    switch (escolha){
+    case 1:
+        soma();
+        break;
+    case 2:
+        subtracao();
+        break;
+    case 3:
+        multiplicacao();
+        break;
+    case 4:
+        divisao();
+        break;
+    case 5:
+        modulo();
+        break;
+    
+    default:
+        printf("Insira o n%cmero corresponde ao da opera%c%co que desejas trabalhar.",u,c,a);
+        break;
+    }
+
+    return 0;
+}
