d1b5313746542cfef36899934e9598dab26ee17d
Henrique <henriquevirgilino@ucl.br>
2022-04-05 18:01:23 -0300

    Adição das funções de conversão

--- main.c
+++ main.c
@@ -2,13 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-void conversor(){
+
+void resultado(int z){
+
+    char resultado[8];
+    itoa(z, resultado, 2); // converte de volta para binário
+
+    int diff = 8 - strlen(resultado); // calcula diferença de caracteres para preenchimento de 0s
+
+    int c;
+    for(c = 0; c < diff; c++){
+        printf("0");
+    }
+
+    printf("%s", resultado);
 
 }
 
-void resultado(){
+int conversor(){
+    //converte para decimal para a realização das operações
+    int x;
+    scanf("%d", &x);
+    int resto;
+    int convertido = 0;
+    int produto = 1;
+
+
+    while(x>0){
+        resto = x % 10;
+        convertido += resto * produto;
+        x /= 10;
+        produto *= 2;
+    }
+
+    return convertido;
 
 }
+
 
 void soma(){
 
@@ -40,10 +70,10 @@ int main(){
     int o = 162; // código dec da tabela ascii para "ó"
     int mod = 37; // código dec da tabela ascii para "%"
 
-    printf("Selecione o operador de acordo com o n%cmero apresentado.\n1: + (Soma)\n2: - (Subtra%c%co)\n3: * (Multiplica%c%co)\n4: / (Divis%co)\n5: %c (M%cdulo)\n\nAp%cs isso, basta inserir os n%cmeros (em bin%crio) para receber o resultado.",u,c,a,c,a,a,mod,o,o,u,A);
+    printf("Selecione o operador de acordo com o n%cmero apresentado.\n1: + (Soma)\n2: - (Subtra%c%co)\n3: * (Multiplica%c%co)\n4: / (Divis%co)\n5: %c (M%cdulo)\n\nAp%cs isso, basta inserir os n%cmeros (em bin%crio) para receber o resultado.\n",u,c,a,c,a,a,mod,o,o,u,A);
 
     int escolha; // recebendo o número da operação
-    scanf("%d", escolha);
+    scanf("%d", &escolha);
 
     switch (escolha){
     case 1:
